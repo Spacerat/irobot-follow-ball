b685ab2a228d21b5eb3bb34da72345a16fdabf6a
Spacerat <spacerat3004@gmail.com>
2012-04-27 08:42:09 +0100

    Beginnings of vision code

---
+++ vision.c
@@ -0,0 +1,76 @@
+#define RED_MIN 128
+#define RED_MUL 4
+#define BLUEGREEN_MUL 3
+#define AREA_MIN 4000
+
+
+static CvCapture * camera;
+static IplImage * image;
+
+/*
+We're going to need some kind of callibrations stuff, but I'm
+not sure exactly how yet.
+*/
+// void callibrate();
+
+/*
+	Basically, this should take the output of image_process
+	as input, and return the angle and distance to the ball.
+	
+	This is probably going to take a lot of tuning to get right.
+*/
+void get_heading(int xpos, int area, float * angle, int * distance) {
+
+}
+
+void vision_init() {
+	camera = cvCreateCameraCapture(0);
+	assert(camera);
+	image=cvQueryFrame(camera);
+	assert(image);
+}
+
+
+    
+
+/*
+Process an IplImage (from the camera) and get values for
+the X position of the ball on the screen, and its size.
+*/
+void image_process(IplImag *image, int * xpos, int * area) {
+    //IplImage *image=cvQueryFrame(camera);
+    unsigned char * pixel_data = (unsigned char *)(image->imageData);
+    int w = image->width;
+    int h = image->height;
+    int c = image->nChannels;
+    int area = 0;
+    int moment = 0;
+    int y = 0;
+    while (y<h) {
+      int x = 0;
+      while (x < w) {
+        unsigned char * blue  = pixel_data;
+        unsigned char * green = pixel_data + 1;
+        unsigned char * red   = pixel_data + 2;
+        if (((RED_MUL*(int)*red) > (BLUEGREEN_MUL*((int)*blue + (int)* green))) 
+            & (*red > RED_MIN)) {
+          //*red = 255;
+          area = area +1;
+          moment = moment + x; 
+        } else {
+          //*red = 0;
+        }
+        //*blue = 0;
+        //*green = 0;
+        pixel_data = pixel_data + c; 
+        x = x + 1;
+      }
+      y = y + 1;
+    }
+    if (area > AREA_MIN) {
+      int xpos = moment/area;
+      return 0;
+    }
+    return 1;
+}
+
